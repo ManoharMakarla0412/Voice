0e4bade4b2e047909fdaf2f88a6d5f42d623968b
ManoharMakarla0412 <manohar@symfor.com>
2025-01-27 22:22:43 +0530

    updated

--- backend/controllers/phonepeContrroller.js
+++ backend/controllers/phonepeContrroller.js
@@ -73,7 +73,8 @@ const checkStatus = async (req, res) => {
   };
 
   try {
-    const response = await fetchData(`${phonepeConfig.MERCHANT_STATUS_URL}/${phonepeConfig.MERCHANT_ID}/${merchantTransactionId}`, options);
+    const response = await fetchData(`${phonepeConfig.MERCHANT_BASE_URL}/${phonepeConfig.MERCHANT_ID}/${merchantTransactionId}`, options);
+    console.log(response);
     if (response.success === true) {
       return res.redirect(phonepeConfig.SUCCESS_URL);
     } else {
