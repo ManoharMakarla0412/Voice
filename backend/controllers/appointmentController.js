c0c96da0fb89cb5daaa32de1d850b3bd42b4bbb3
ManoharMakarla0412 <manohar@symfor.com>
2025-03-11 16:23:04 +0530

    Update appointmentController.js

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,8 +1,8 @@
-
-
-
 // Handle appointment booking
 exports.createAppointment = async (req, res) => {
- console.log("Recieved Request: ", req.body);
-  res.status(200).json({req.body});
+    console.log("Received Request:");
+    Object.keys(req.body).forEach(key => {
+        console.log(`${key}: ${req.body[key]}`);
+    });
+    res.status(200).json(req.body);
 };
