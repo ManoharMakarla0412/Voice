e88e1461d81e2d2d0b665026fdce43dc79390d7a
ManoharMakarla0412 <manohar@symfor.com>
2025-04-08 16:59:43 +0530

    Update constants.ts

--- frontend/app/utils/constants.ts
+++ frontend/app/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'https://osaw.in/v1/voice';
 //export const BASE_URL = "http://localhost:5003";
 // export const BASE_URL = "https://voice-i3n0.onrender.com"
-export const BASE_URL = 'https://osaw.in/v1/dev4/voice';
+export const BASE_URL = 'https://osaw.in/v1/dev5';
